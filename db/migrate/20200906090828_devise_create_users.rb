81ce0e5319ca290a09bc248bb05ec7b64b4d35c0
bata1016 <blp0087bata@gmail.com>
2020-09-07 13:10:09 +0900

    トップページからdeviseへのルーティング

--- db/migrate/20200906090828_devise_create_users.rb
+++ db/migrate/20200906090828_devise_create_users.rb
@@ -9,7 +9,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :last_name_kana,     null: false
       t.string :first_name_kann,    null: false
       t.string :nick_name,          null: false
-      t.date   :birth_day,          null: false
+      t.date   :birth_day,          null: false 
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 

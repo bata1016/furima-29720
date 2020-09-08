03b74721044d8c988142ed2a444782c64bbb5693
bata1016 <blp0087bata@gmail.com>
2020-09-08 14:34:09 +0900

    Userモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,13 @@ class User < ApplicationRecord
   
 
   with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters'} do
-    validates :last_name_kana
-    validates :first_name_kana
     validates :last_name     
     validates :first_name    
+  end
+
+  with_options presence: true, format: {with: /\A[ァ-ン]+\z/, message: 'Full-width katakana'} do
+    validates :last_name_kana
+    validates :first_name_kana
   end
 
   

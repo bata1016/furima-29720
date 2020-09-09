afc0b853575b1f65ef2b2fbc50aa279d404a6a24
bata1016 <blp0087bata@gmail.com>
2020-09-09 23:55:54 +0900

    整数出力に訂正

--- app/javascript/add_tax.js
+++ app/javascript/add_tax.js
@@ -2,8 +2,8 @@
   let item_price = document.getElementById('item-price');
   item_price.addEventListener('input', function() { 
     let tax_price = item_price.value * 0.1
-    let profit = item_price.value - tax_price
-    document.getElementById('add-tax-price').innerHTML = tax_price
+    let profit = item_price.value - parseInt(tax_price)
+    document.getElementById('add-tax-price').innerHTML = parseInt(tax_price)
     document.getElementById('profit').innerHTML = profit
   })
   }

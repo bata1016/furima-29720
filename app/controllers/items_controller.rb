e38f551bce182f32f11e8170b2d8a181a34775ee
bata1016 <blp0087bata@gmail.com>
2020-09-11 20:18:58 +0900

    before_actionへのメソッド集約

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:update,:show,:edit]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -18,16 +19,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     @image = @item.image
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_parameter)
       redirect_to root_path
     else
@@ -45,5 +43,9 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end

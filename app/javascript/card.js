4e19f43fbedbb91356fe5f62f23f4fc0b117d972
bata1016 <blp0087bata@gmail.com>
2020-09-14 12:12:00 +0900

    token生成機能の実装

---
+++ app/javascript/card.js
@@ -0,0 +1,36 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_XXXXXXXXX");
+  const form = document.getElementById('charge-form');
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    // なぜインスタンスを生成している？
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("card-number"),
+      exp_month: formData.get("card-exp-month"),
+      exp_year: `20${formData.get("card-exp-year")}`,
+      cvc: formData.get("card-cvc")
+    };
+    Payjp.crateToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementsById("charge-form");
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+      // なぜname属性を取り除いている？
+      document.getElementById("card-number").removeAttribute('name');
+      document.getElementById("card-exp-year").removeAttribute('name');
+      document.getElementById("card-exp-month").removeAttribute('name');
+      document.getElementById('card-cvc').removeAttribute('name');
+
+      document.getElementById('charge-form').submit();
+      document.getElementById('charge-form').reset();
+    });
+  });
+};
+
+window.addEventListener("load", pay)
